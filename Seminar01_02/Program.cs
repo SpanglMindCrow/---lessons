1ee3150b1b703142d4daf02b71bd0824f0a9d288
SpanglMindCrow <asova417@gmail.com>
2022-04-16 14:56:01 +0500

    add Seminar_01. Task 2.

---
+++ Seminar01_02/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите перовое чило: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе чило: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+int square = numberA * numberA;
+
+if (square == numberB)
+{
+    Console.WriteLine("Да, первое число является квадратом второго");
+}
+else
+{
+    Console.WriteLine("Неть, первое число не является квадратом второго");
+}
